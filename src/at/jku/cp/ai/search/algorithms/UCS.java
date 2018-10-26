e1867cc6c5262647702f073418bf925ccddb955d
Pichlerdom <pichlerdominik97@gmail.com>
2018-10-26 12:41:19 +0200

    UCS geht!

--- src/at/jku/cp/ai/search/algorithms/UCS.java
+++ src/at/jku/cp/ai/search/algorithms/UCS.java
@@ -5,6 +5,9 @@ import java.util.function.Predicate;
 
 import at.jku.cp.ai.search.Node;
 import at.jku.cp.ai.search.Search;
+import at.jku.cp.ai.search.datastructures.Pair;
+import at.jku.cp.ai.search.datastructures.StablePriorityQueue;
+import at.jku.cp.ai.search.datastructures.StackWithFastContains;
 
 // Uniform Cost Search
 public class UCS implements Search
@@ -20,6 +23,29 @@ public class UCS implements Search
 	public Node search(Node start, Predicate<Node> endPredicate)
 	{
 		
+		StablePriorityQueue<Double, Node> queue = new StablePriorityQueue<>();
+		StackWithFastContains<Integer> path = new StackWithFastContains<>();
+		queue.add(new Pair<Double, Node>(cost.apply(start), start));
+		path.push(start.hashCode());
+		
+		while(!queue.isEmpty())
+		{
+			if(endPredicate.test(queue.element().s)) {
+				return queue.element().s;
+			}
+			
+			Pair<Double,Node> curr = queue.remove();
+			
+			for(Node node : curr.s.adjacent()){
+				//System.out.println(path.contains(node.hashCode()));
+				if(!path.contains(node.hashCode())){
+					path.push(node.hashCode());
+					queue.add(new Pair<Double, Node>(cost.apply(node) + curr.f, node));
+				}
+				
+			}
+		}
 		return null;
 	}
+		
 }
