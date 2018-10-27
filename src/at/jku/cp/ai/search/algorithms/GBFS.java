5a9c6454825ad1c7229a65eca328a481d29d0078
Pichlerdom <pichlerdominik97@gmail.com>
2018-10-27 21:06:07 +0200

    GBFS definitely works!!!!!

--- src/at/jku/cp/ai/search/algorithms/GBFS.java
+++ src/at/jku/cp/ai/search/algorithms/GBFS.java
@@ -1,10 +1,16 @@
 package at.jku.cp.ai.search.algorithms;
 
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+
+
 import at.jku.cp.ai.search.Node;
 import at.jku.cp.ai.search.Search;
+import at.jku.cp.ai.search.datastructures.Pair;
+import at.jku.cp.ai.search.datastructures.StablePriorityQueue;
+import at.jku.cp.ai.search.datastructures.StackWithFastContains;
 
 // Greedy Best-First Search
 public class GBFS implements Search
@@ -19,7 +25,31 @@ public class GBFS implements Search
 	@Override
 	public Node search(Node start, Predicate<Node> endPredicate)
 	{
-		// TODO, assignment 1
+		StablePriorityQueue<Double, Node> queue = new StablePriorityQueue<>();
+		StackWithFastContains<Integer> path = new StackWithFastContains<>();
+		
+		
+		Node curr = start;
+		
+		path.push(start.hashCode());
+		
+		do {
+			
+			if(endPredicate.test(curr)){
+				return curr;
+			}
+			
+			List<Node> adjacentNodes = curr.adjacent();
+			for(Node node : adjacentNodes) {
+				if(!path.contains(node.hashCode())) {
+					queue.add(new Pair<Double, Node>(heuristic.apply(node), node));
+					path.push(node.hashCode());
+				}
+			}
+			
+			curr = queue.remove().s;	
+		} while (!queue.isEmpty());
+		
 		return null;
 	}
 }
